ddb100cb94c2058c68fec1191ace824e63dce456
kinoko0518 <t4keda6osci01a@gmail.com>
2026-09-14 22:18:39 +0900

    refactor(xhtml): builder.rsをframeおよびdecoサブモジュールに分割

--- aozora-rs/aozora-rs-xhtml/src/xhtmlnize/builder.rs
+++ aozora-rs/aozora-rs-xhtml/src/xhtmlnize/builder.rs
@@ -1,85 +1,24 @@
+mod deco;
+mod frame;
+
 use std::borrow::Cow;
 use std::vec::IntoIter;
 
-use aozora_rs_core::{BosenKind, BotenKind, Deco, Retokenized};
+use aozora_rs_core::Retokenized;
 use itertools::MultiPeek;
 
 use crate::{
     CDepth, Chapter,
-    xhtmlnize::ast::{BlockNode, ContentNode, HeadingLevel, InlineNode},
+    xhtmlnize::ast::{BlockNode, InlineNode},
 };
-
-enum ContainerFrame<'s> {
-    Div {
-        attributes: Vec<Cow<'s, str>>,
-        children: Vec<ContentNode<'s>>,
-    },
-    Paragraph {
-        attributes: Vec<Cow<'s, str>>,
-        children: Vec<InlineNode<'s>>,
-    },
-    Heading {
-        level: HeadingLevel,
-        attributes: Vec<Cow<'s, str>>,
-        children: Vec<InlineNode<'s>>,
-    },
-    Span {
-        attributes: Vec<Cow<'s, str>>,
-        children: Vec<InlineNode<'s>>,
-    },
-    Ruby {
-        children: Vec<InlineNode<'s>>,
-    },
-    Sup {
-        attributes: Vec<Cow<'s, str>>,
-        children: Vec<InlineNode<'s>>,
-    },
-    Sub {
-        attributes: Vec<Cow<'s, str>>,
-        children: Vec<InlineNode<'s>>,
-    },
-}
-
-impl<'s> ContainerFrame<'s> {
-    fn is_inline_context(&self) -> bool {
-        matches!(
-            self,
-            Self::Paragraph { .. }
-                | Self::Heading { .. }
-                | Self::Span { .. }
-                | Self::Ruby { .. }
-                | Self::Sup { .. }
-                | Self::Sub { .. }
-        )
-    }
-
-    fn push_inline(&mut self, inline: InlineNode<'s>) {
-        match self {
-            Self::Div { children, .. } => children.push(ContentNode::Inline(inline)),
-            Self::Paragraph { children, .. } => children.push(inline),
-            Self::Heading { children, .. } => children.push(inline),
-            Self::Span { children, .. } => children.push(inline),
-            Self::Ruby { children } => children.push(inline),
-            Self::Sup { children, .. } => children.push(inline),
-            Self::Sub { children, .. } => children.push(inline),
-        }
-    }
-
-    fn push_block(&mut self, block: BlockNode<'s>) {
-        match self {
-            Self::Div { children, .. } => children.push(ContentNode::Block(block)),
-            // インラインコンテキストにはブロックは入れられない（型が保証）
-            _ => unreachable!("Block elements cannot be pushed into inline context"),
-        }
-    }
-}
+use frame::ContainerFrame;
 
 pub struct TreeBuilder<'s> {
-    stack: Vec<ContainerFrame<'s>>,
+    pub(crate) stack: Vec<ContainerFrame<'s>>,
     pub chapters: Vec<Chapter>,
     pub dependencies: Vec<String>,
-    c_depth: CDepth,
-    xhtml_id: usize,
+    pub(crate) c_depth: CDepth,
+    pub(crate) xhtml_id: usize,
 }
 
 impl<'s> TreeBuilder<'s> {
@@ -107,7 +46,7 @@ impl<'s> TreeBuilder<'s> {
         self.c_depth.clone()
     }
 
-    fn is_in_inline_context(&self) -> bool {
+    pub(crate) fn is_in_inline_context(&self) -> bool {
         self.stack.iter().rev().any(|f| f.is_inline_context())
     }
 
@@ -117,11 +56,11 @@ impl<'s> TreeBuilder<'s> {
         }
     }
 
-    fn push_frame(&mut self, frame: ContainerFrame<'s>) {
+    pub(crate) fn push_frame(&mut self, frame: ContainerFrame<'s>) {
         self.stack.push(frame);
     }
 
-    fn pop_frame(&mut self) -> Option<ContainerFrame<'s>> {
+    pub(crate) fn pop_frame(&mut self) -> Option<ContainerFrame<'s>> {
         // ルート要素はpopしない
         if self.stack.len() > 1 {
             self.stack.pop()
@@ -130,7 +69,7 @@ impl<'s> TreeBuilder<'s> {
         }
     }
 
-    fn close_frame_and_attach(&mut self) {
+    pub(crate) fn close_frame_and_attach(&mut self) {
         if let Some(popped) = self.pop_frame() {
             if let Some(parent) = self.stack.last_mut() {
                 match popped {
@@ -191,7 +130,6 @@ impl<'s> TreeBuilder<'s> {
                         });
                     }
                     ContainerFrame::Ruby { children } => {
-                        // ルビはrtが必要なので、明示的なclose_rubyで処理される
                         parent.push_inline(InlineNode::Span {
                             attributes: Vec::new(),
                             children,
@@ -202,7 +140,7 @@ impl<'s> TreeBuilder<'s> {
         }
     }
 
-    fn close_ruby(&mut self, rt: Cow<'s, str>) {
+    pub(crate) fn close_ruby(&mut self, rt: Cow<'s, str>) {
         if let Some(popped) = self.pop_frame() {
             if let ContainerFrame::Ruby { children } = popped {
                 if let Some(parent) = self.stack.last_mut() {
@@ -212,270 +150,9 @@ impl<'s> TreeBuilder<'s> {
                     });
                 }
             } else {
-                // ルビ以外のフレームだった場合は元に戻してattach
                 self.stack.push(popped);
                 self.close_frame_and_attach();
             }
-        }
-    }
-
-    fn parse_chapter<F>(
-        &mut self,
-        peekable: &mut MultiPeek<IntoIter<Retokenized<'s>>>,
-        end_variant: Deco,
-        inc_method: F,
-    ) -> Chapter
-    where
-        F: FnOnce(&mut CDepth),
-    {
-        inc_method(&mut self.c_depth);
-        let mut buff = String::new();
-
-        while let Some(s) = peekable.peek() {
-            match s {
-                Retokenized::DecoEnd(d) if d == &end_variant => break,
-                Retokenized::Text(t) => buff.extend(t.chars()),
-                _ => (),
-            }
-        }
-        peekable.reset_peek();
-
-        Chapter {
-            xhtml_id: self.xhtml_id,
-            name: buff,
-            depth: self.c_depth.clone(),
-        }
-    }
-
-    pub fn handle_deco_begin(
-        &mut self,
-        peekable: &mut MultiPeek<IntoIter<Retokenized<'s>>>,
-        d: Deco<'s>,
-    ) {
-        match d {
-            Deco::AHead => {
-                let chapter = self.parse_chapter(peekable, Deco::AHead, |c| c.increament_a());
-                self.push_frame(ContainerFrame::Heading {
-                    level: HeadingLevel::H1,
-                    attributes: vec![
-                        Cow::Borrowed("class=\"a_head\""),
-                        Cow::Owned(format!("id=\"{}\"", chapter.get_id())),
-                    ],
-                    children: Vec::new(),
-                });
-                self.chapters.push(chapter);
-            }
-            Deco::BHead => {
-                let chapter = self.parse_chapter(peekable, Deco::BHead, |c| c.increament_b());
-                self.push_frame(ContainerFrame::Heading {
-                    level: HeadingLevel::H2,
-                    attributes: vec![
-                        Cow::Borrowed("class=\"b_head\""),
-                        Cow::Owned(format!("id=\"{}\"", chapter.get_id())),
-                    ],
-                    children: Vec::new(),
-                });
-                self.chapters.push(chapter);
-            }
-            Deco::CHead => {
-                let chapter = self.parse_chapter(peekable, Deco::CHead, |c| c.increament_c());
-                self.push_frame(ContainerFrame::Heading {
-                    level: HeadingLevel::H3,
-                    attributes: vec![
-                        Cow::Borrowed("class=\"c_head\""),
-                        Cow::Owned(format!("id=\"{}\"", chapter.get_id())),
-                    ],
-                    children: Vec::new(),
-                });
-                self.chapters.push(chapter);
-            }
-            Deco::Bold => {
-                self.push_frame(ContainerFrame::Span {
-                    attributes: vec![Cow::Borrowed("class=\"bold\"")],
-                    children: Vec::new(),
-                });
-            }
-            Deco::Italic => {
-                self.push_frame(ContainerFrame::Span {
-                    attributes: vec![Cow::Borrowed("class=\"italic\"")],
-                    children: Vec::new(),
-                });
-            }
-            Deco::Ruby(_) | Deco::Mama => {
-                self.push_frame(ContainerFrame::Ruby {
-                    children: Vec::new(),
-                });
-            }
-            Deco::Bosen(b) => {
-                self.push_frame(ContainerFrame::Span {
-                    attributes: match b {
-                        BosenKind::Chain => vec![Cow::Borrowed("class=\"bosen-chain\"")],
-                        BosenKind::Plain => vec![Cow::Borrowed("class=\"bosen-solid\"")],
-                        BosenKind::Double => vec![Cow::Borrowed("class=\"bosen-double\"")],
-                        BosenKind::Dashed => vec![Cow::Borrowed("class=\"bosen-dashed\"")],
-                        BosenKind::Wavy => vec![Cow::Borrowed("class=\"bosen-wavy\"")],
-                    },
-                    children: Vec::new(),
-                });
-            }
-            Deco::Boten(b) => {
-                self.push_frame(ContainerFrame::Span {
-                    attributes: match b {
-                        BotenKind::Circle => vec![Cow::Borrowed("class=\"circle\"")],
-                        BotenKind::CircleFilled => vec![Cow::Borrowed("class=\"circle-filled\"")],
-                        BotenKind::Sesame => vec![Cow::Borrowed("class=\"sesame\"")],
-                        BotenKind::DoubleCircle => vec![Cow::Borrowed("class=\"double-circle\"")],
-                        BotenKind::Hebinome => vec![Cow::Borrowed("class=\"hebinome\"")],
-                        BotenKind::Triangle => vec![Cow::Borrowed("class=\"triangle\"")],
-                        BotenKind::TriangleFilled => {
-                            vec![Cow::Borrowed("class=\"triangle-filled\"")]
-                        }
-                        BotenKind::Crossing => vec![Cow::Borrowed("class=\"crossing\"")],
-                    },
-                    children: Vec::new(),
-                });
-            }
-            Deco::Indent(i) => {
-                let attr = vec![Cow::Owned(format!("style=\"padding-inline-start: {}em;\"", i))];
-                if self.is_in_inline_context() {
-                    self.push_frame(ContainerFrame::Span {
-                        attributes: attr,
-                        children: Vec::new(),
-                    });
-                } else {
-                    self.push_frame(ContainerFrame::Div {
-                        attributes: attr,
-                        children: Vec::new(),
-                    });
-                }
-            }
-            Deco::Hanging((h, j)) => {
-                let attr = vec![Cow::Owned(format!(
-                    "style=\"padding-inline-start: {}em; text-indent: {}em;\"",
-                    j,
-                    (h as i32) - (j as i32)
-                ))];
-                if self.is_in_inline_context() {
-                    self.push_frame(ContainerFrame::Span {
-                        attributes: attr,
-                        children: Vec::new(),
-                    });
-                } else {
-                    self.push_frame(ContainerFrame::Div {
-                        attributes: attr,
-                        children: Vec::new(),
-                    });
-                }
-            }
-            Deco::Grounded => {
-                self.push_frame(ContainerFrame::Paragraph {
-                    attributes: vec![Cow::Borrowed("class=\"grounded\"")],
-                    children: Vec::new(),
-                });
-            }
-            Deco::LowFlying(l) => {
-                self.push_frame(ContainerFrame::Paragraph {
-                    attributes: vec![Cow::Owned(format!(
-                        "style=\"text-align: right; padding-inline-end: {}em;\"",
-                        l
-                    ))],
-                    children: Vec::new(),
-                });
-            }
-            Deco::HinV => {
-                self.push_frame(ContainerFrame::Span {
-                    attributes: vec![Cow::Borrowed("class=\"hinv\"")],
-                    children: Vec::new(),
-                });
-            }
-            Deco::Bigger(b) => {
-                self.push_frame(ContainerFrame::Span {
-                    attributes: vec![Cow::Borrowed(match b {
-                        1 => "style=\"font-size: large\"",
-                        2 => "style=\"font-size: x-large\"",
-                        _ => "style=\"font-size: xx-large\"",
-                    })],
-                    children: Vec::new(),
-                });
-            }
-            Deco::Smaller(b) => {
-                self.push_frame(ContainerFrame::Span {
-                    attributes: vec![Cow::Borrowed(match b {
-                        1 => "style=\"font-size: small\"",
-                        2 => "style=\"font-size: x-small\"",
-                        _ => "style=\"font-size: xx-small\"",
-                    })],
-                    children: Vec::new(),
-                });
-            }
-            Deco::VHCentre => {
-                let attr = vec![Cow::Borrowed("class=\"vhcentre\"")];
-                if self.is_in_inline_context() {
-                    self.push_frame(ContainerFrame::Span {
-                        attributes: attr,
-                        children: Vec::new(),
-                    });
-                } else {
-                    self.push_frame(ContainerFrame::Div {
-                        attributes: attr,
-                        children: Vec::new(),
-                    });
-                }
-            }
-            Deco::Warichu => {
-                self.push_frame(ContainerFrame::Span {
-                    attributes: vec![Cow::Borrowed("class=\"warichu\"")],
-                    children: Vec::new(),
-                });
-            }
-            Deco::HorizontalLayout => {
-                let attr = vec![Cow::Borrowed("class=\"horizontal-block\"")];
-                if self.is_in_inline_context() {
-                    self.push_frame(ContainerFrame::Span {
-                        attributes: attr,
-                        children: Vec::new(),
-                    });
-                } else {
-                    self.push_frame(ContainerFrame::Div {
-                        attributes: attr,
-                        children: Vec::new(),
-                    });
-                }
-            }
-            Deco::Kerning(k) => {
-                let attr = vec![Cow::Owned(format!("style=\"max-inline-size: {}em;\"", k))];
-                if self.is_in_inline_context() {
-                    self.push_frame(ContainerFrame::Span {
-                        attributes: attr,
-                        children: Vec::new(),
-                    });
-                } else {
-                    self.push_frame(ContainerFrame::Div {
-                        attributes: attr,
-                        children: Vec::new(),
-                    });
-                }
-            }
-            Deco::Sub => {
-                self.push_frame(ContainerFrame::Sub {
-                    attributes: Vec::new(),
-                    children: Vec::new(),
-                });
-            }
-            Deco::Sup => {
-                self.push_frame(ContainerFrame::Sup {
-                    attributes: Vec::new(),
-                    children: Vec::new(),
-                });
-            }
-        }
-    }
-
-    pub fn handle_deco_end(&mut self, e: Deco<'s>) {
-        match e {
-            Deco::Ruby(r) => self.close_ruby(Cow::Borrowed(r)),
-            Deco::Mama => self.close_ruby(Cow::Borrowed("ママ")),
-            _ => self.close_frame_and_attach(),
         }
     }
 
